0a1743f47c4a04db7a680ba101dda4db33ecb2e2
Ksushondrik <ksushondrik369@gmail.com>
2024-01-28 02:33:28 +0200

    У другому завданні додано ввід даних;  написано четверте завдання

--- task_2.py
+++ task_2.py
@@ -1,5 +1,9 @@
 import random
 
+min = int(input("Введіть мінімальне значення діапазону вибірки: "))
+max = int(input("Введіть максимальне значення діапазону вибірки:"))
+quantity = int(input("Введіть кількість чисел у виграшній комбінації: "))
+
 def get_numbers_ticket(min: int, max: int, quantity: int) -> list:                              # створюємо функцію вибірки
     sample = []
     if (min < 1) or (max > 1000) or (max <= quantity <= min) or (quantity >= (max-min)):        # перевіряємо задані параметри
@@ -12,4 +16,4 @@ def get_numbers_ticket(min: int, max: int, quantity: int) -> list:              
     sample.sort()                                                                               # відсортовуємо за зростанням
     return sample
 
-print(get_numbers_ticket(1,10,6))
+print(get_numbers_ticket(min, max, quantity))
